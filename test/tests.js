13a44352cbbe2a4ca3afd71264f03607bf3adae6
Chris Bargren <cbargren@gmail.com>
2016-08-12 16:10:40 -0700

    Babelifying, linting and replacing mongo with mongoose

--- test/tests.js
+++ test/tests.js
@@ -1,96 +1,67 @@
-var expect = require('expect.js');
+/* eslint-disable prefer-arrow-callback */
 
-var MongoStore = require('../');
-var getDb = require('mongo-getdb');
-getDb.init('default', 'mongodb://localhost/test_brute_express_mongo');
+const expect = require('expect');
+const mongoose = require('mongoose');
 
-var mongoStore, collection;
+const Store = require('../dist');
+const schema = require('../dist/schema');
 
-describe('MongoStore', function () {
-  beforeEach(function (done) {
-    mongoStore = new MongoStore(function (callback) {
-      getDb(function (db) {
-        collection = db.collection('api_limits');
-        collection.remove({}, function () {
-          callback(collection);
-          done();
+mongoose.connect('mongodb://localhost:27017/test_brute_express_mongoose');
+
+describe('MongooseStore', function () {
+  beforeEach(function () {
+    mongoose.Promise = Promise;
+    this.model = mongoose.model('bruteforce', schema);
+    this.store = new Store(callback => {
+      callback(this.model);
+    });
+    return this.model.remove({}).exec();
+  });
+
+  it('should be able to set a value', function () {
+    return this.store.set('foo', { count: 123 }, 1000)
+      .then(() => this.model.findOne({ _id: 'foo' }).exec())
+      .then(data => {
+        expect(data.data).toMatch({
+          count: 123
+        });
+        expect(data.expires).toBeA(Date);
+      });
+  });
+
+  it('should be able to get a value', function () {
+    return this.store.set('foo', { count: 123 }, 1000)
+      .then(() => this.store.get('foo'))
+      .then(data => {
+        expect(data).toMatch({
+          count: 123
         });
       });
-    });
   });
 
-  it('should be able to set a value', function (done) {
-    mongoStore.set('foo', {bar:123}, 1000, function (err) {
-      if (err) return done(err);
-      collection.findOne({_id: 'foo'}, function (err, limit) {
-        if (err) return done(err);
-        expect(limit.data).have.property('bar');
-        expect(limit.expires).to.be.a(Date);
-        done();
+  it('should return undef if expired', function () {
+    return this.store.set('foo', { count: 123 }, 0)
+      .then(() => this.store.get('foo'))
+      .then(data => {
+        expect(data).toBe(null);
       });
-    });
   });
 
-  it('should be able to get a value', function (done) {
-    mongoStore.set('foo', {bar:123}, 1000, function (err) {
-      if (err) return done(err);
-      mongoStore.get('foo', function (err, data) {
-        if (err) return done(err);
-        expect(data).have.property('bar');
-        done();
+  it('should delete the doc if expired', function () {
+    return this.store.set('foo', { count: 123 }, 0)
+      .then(() => this.store.get('foo'))
+      .then(() => this.model.findOne({ _id: 'foo' }).exec())
+      .then(data => {
+        expect(data).toBe(null);
       });
-    });
   });
 
-  it('should return undef if expired', function (done) {
-    mongoStore.set('foo', { bar: 123 }, 0, function (err) {
-      if (err) return done(err);
-      setTimeout(function () {
-
-        mongoStore.get('foo', function (err, data) {
-          if (err) return done(err);
-          expect(data).to.be(undefined);
-          done();
-        });
-
-      }, 200);
-    });
-  });
-
-  it('should delete the doc if expired', function (done) {
-    mongoStore.set('foo', { bar: 123 }, 0, function (err) {
-      if (err) return done(err);
-      setTimeout(function () {
-        mongoStore.get('foo', function (err, data) {
-
-          setTimeout(function () {
-
-            collection.findOne({ _id: 'foo' }, function (err, d) {
-              expect(d).to.be(null);
-              done();
-            });
-
-          }, 100);
-
-        });
-      }, 100);
-    });
-  });
-
-
-  it('should be able to reset', function (done) {
-    mongoStore.set('foo', {bar:123}, 1000, function (err) {
-      if (err) return done(err);
-      mongoStore.reset('foo', function (err) {
-        if (err) return done(err);
-
-        collection.findOne({_id: 'foo'}, function (err, limit) {
-          if (err) return done(err);
-          expect(limit).to.be(null);
-          done();
-        });
+  it('should be able to reset', function () {
+    return this.store.set('foo', { count: 123 }, 1000)
+      .then(() => this.store.reset('foo'))
+      .then(() => this.model.findOne({ _id: 'foo' }).exec())
+      .then(data => {
+        expect(data).toBe(null);
       });
-    });
   });
-
 });
